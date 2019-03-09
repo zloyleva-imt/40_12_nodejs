eab398a2e865aed5fe5a97439fe39433c19c2e10
ZloyLeva <zloyleva@gmail.com>
2019-03-09 12:17:02 +0200

    add Routes

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-
-let products = [];
-
 const express = require('express');
 const app = express();
 
@@ -21,112 +18,12 @@ const db = new sqlite.Database(dBPath, (err) => {
     console.log('\x1b[36m%s\x1b[0m', 'App was connected to DB');
 });
 
-
-
 app.get('/', (req, res) => {
     res.send("Shoes shop\n");
 });
 
-app.get('/products', (req, res) => {
-    console.log('\x1b[45m',"detected /products query");
-
-    db.all("SELECT * FROM products", (err, rows) => {
-        if (err) {
-            console.log('\x1b[31m', err.message);
-        }
-        res.json({
-            status: "ok",
-            data:{
-                products: rows,
-            }
-        });
-    });
-});
-
-app.get('/products/:id', (req, res) => {
-
-    const {params : {id}} = req;
-
-    if( Number.isInteger(parseInt(id)) && parseInt(id) > 0){
-        db.get(`SELECT * FROM products WHERE id = ${id}`,(err, row) => {
-            if (err) {
-                console.log('\x1b[31m', err.message);
-            }
-
-            console.log(row);
-
-            res.json({
-                status: "ok",
-                data:{
-                    products: row,
-                }
-            });
-        });
-    }else {
-        res.json({
-            status: "404",
-            error:{
-                message: "Products not found. Not correct ID",
-            }
-        });
-    }
-});
-
-app.put('/products/:id', (req, res) => {
-    const {params : {id}} = req;
-    console.log(req.body);
-
-    let reqArray = [];
-    const {body} = req;
-    for (let prop in body) {
-        reqArray.push(`${prop} = '${body[prop]}'`)
-    }
-
-    console.log(reqArray.join(", "));
-
-    const query = `UPDATE products SET ${reqArray.join(", ")} WHERE id = ${id};`;
-    db.run(query, (err, data) => {
-        if (err) {
-            console.log('\x1b[31m', err.message);
-        }
-
-        res.json({
-            status: "updated",
-        });
-    });
-});
-
-app.post('/products', (req, res) => {
-    console.log(req.body);
-
-    const {price, title, description, image, amount} = req.body;
-
-    const query = `INSERT INTO products(price,title,description,image,amount) VALUES('${price}', '${title}', '${description}', '${image}', '${amount}')`;
-    db.run(query, (err, data) => {
-        if (err) {
-            console.log('\x1b[31m', err.message);
-        }
-        console.log('\x1b[36m%s\x1b[0m', data);
-    });
-
-    res.send("Done\n");
-});
-
-app.delete('/products/:id', (req, res) => {
-    const {params: {id}} = req;
-
-    const query =`DELETE FROM products WHERE id = ${id}`;
-    db.run(query, (err, data) => {
-        if (err) {
-            console.log('\x1b[31m', err.message);
-        }
-
-        res.json({
-            status: "delete",
-        });
-    });
-});
-
+const products = require('./routes/products')(db);
+app.use('/products', products);
 
 app.listen(3000, err => {
     if(err){
