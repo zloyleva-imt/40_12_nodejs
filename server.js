96fe8daa7cb02cf1f33a52b722989e36dad6164b
ZloyLeva <zloyleva@gmail.com>
2019-03-09 14:35:11 +0200

    create check DB connect

--- server.js
+++ server.js
@@ -2,28 +2,43 @@ const express = require('express');
 const app = express();
 
 
-const sqlite = require('sqlite3').verbose();
+// const sqlite = require('sqlite3').verbose();
 const path = require('path');
 const bodyParser = require('body-parser');
 
 // app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-const dBPath = path.resolve(__dirname, 'database/store.sqlite');
-
-const db = new sqlite.Database(dBPath, (err) => {
-    if(err){
-        console.log('\x1b[31m', err.message);
-    }
-    console.log('\x1b[36m%s\x1b[0m', 'App was connected to DB');
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('store', 'root', '123456789root', {
+    host: 'localhost',
+    dialect: 'mysql',
 });
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+
+// const dBPath = path.resolve(__dirname, 'database/store.sqlite');
+//
+// const db = new sqlite.Database(dBPath, (err) => {
+//     if(err){
+//         console.log('\x1b[31m', err.message);
+//     }
+//     console.log('\x1b[36m%s\x1b[0m', 'App was connected to DB');
+// });
 
 app.get('/', (req, res) => {
     res.send("Shoes shop\n");
 });
 
-const products = require('./routes/products')(db);
-app.use('/products', products);
+// const products = require('./routes/products')(db);
+// app.use('/products', products);
 
 app.listen(3000, err => {
     if(err){
