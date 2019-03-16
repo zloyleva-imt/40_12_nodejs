48bea09bd81b9179e53fff0211698ce2f01191df
ZloyLeva <zloyleva@gmail.com>
2019-03-16 14:14:28 +0200

    create add to cart

--- controllers/cartsController.js
+++ controllers/cartsController.js
@@ -18,3 +18,35 @@ exports.cart_list = (req, res) => {
             });
         });
 };
+
+exports.add_to_cart = (req, res) => {
+
+    const {params: {userId,productId}} = req;
+
+    console.log(userId,productId);
+
+    CartItem.findOne({ where: {userId,productId} })
+        .then(function(obj) {
+            if(obj) { // update
+                console.log("update",JSON.stringify(obj));
+                obj.increment('amount');
+                return res.json({
+                    status: "ok",
+                    data:{
+                        cartItems: obj,
+                    }
+                });
+            }
+            else { // insert
+                console.log("insert",JSON.stringify(obj));
+                const newItem = CartItem.create({userId,productId,amount:1});
+                return res.json({
+                    status: "ok",
+                    data:{
+                        cartItems: newItem,
+                    }
+                });
+            }
+        });
+
+};
--- routes/cart.js
+++ routes/cart.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const cartsController = require('../controllers/cartsController');
 
 router.get('/:userId',cartsController.cart_list);
+router.post('/:userId/:productId',cartsController.add_to_cart);
 
 module.exports = router;
